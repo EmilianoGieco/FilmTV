8202784d93b716c28c8b3b0e00767929d23ffe18
AbiSulkes <sulkesabi@gmail.com>
2023-08-31 16:51:44 -0300

    en proceso archivo ultimos estrenos

--- src/controllers/peliculaController.js
+++ src/controllers/peliculaController.js
@@ -10,13 +10,13 @@ const streamifier = require('streamifier');
 
 
 //cloudinary       
-cloudinary.config({ 
-  cloud_name: 'dlimugyad', 
-  api_key: '232418438234228', 
-  api_secret: 'APayZJi7Qqs1U4CbQS2gkE6h1k4' 
+cloudinary.config({
+  cloud_name: 'dlimugyad',
+  api_key: '232418438234228',
+  api_secret: 'APayZJi7Qqs1U4CbQS2gkE6h1k4'
 });
 
-//base de datos
+//base de datos conexion
 let db = require("../database/models");
 const { where } = require('sequelize');
 
@@ -31,25 +31,87 @@ const controlador = {
     res.render("movies/detallePelicula", { movie })
   },
 
- /*prueba de metodo cloudinary*/ 
+  /*prueba de metodo cloudinary*/
 
- postCrearFilm: (req, res) => {
-  const imageBuffer = req.file.buffer;
-  const customFilename = '';
+  postCrearFilm: (req, res) => {
+    const imageBuffer = req.file.buffer;
+    const customFilename = '';
 
-  const stream = cloudinary.uploader.upload_stream({ resource_type: 'image', public_id: customFilename}, (error, result) => {
-    if (error) {
-      console.error('Error:', error);
-    } else {
+    const stream = cloudinary.uploader.upload_stream({ resource_type: 'image', public_id: customFilename }, (error, result) => {
+      if (error) {
+        console.error('Error:', error);
+      } else {
+        const peliculas = JSON.parse(fs.readFileSync(peliculaPath, "utf-8"));
+        console.log(req.body);
+        const nuevoFilm = {
+          id: uuidv4(),
+          nombre: req.body.nombre,
+          imagen: result.secure_url || "/img/error-critico.jpg",
+          descripcion: req.body.descripcion,
+          video: req.body.video,
+          fichatecnica: req.body.fichatecnica
+        };
+        peliculas.push(nuevoFilm);
+        const peliculasJSON = JSON.stringify(peliculas, null, " ");
+        fs.writeFile(peliculaPath, peliculasJSON, error => {
+          if (error) {
+            console.error(error);
+          } else {
+            res.redirect("/");
+          }
+        });
+      }
+    });
+
+    streamifier.createReadStream(imageBuffer).pipe(stream);
+  },
+
+
+
+
+
+  /*carga de producto film (cloudinary)
+  postCrearFilm: (req, res) => {
+ 	
+   const imageBuffer = req.file.buffer;
+   const customFilename = '';
+ 
+   const stream = cloudinary.uploader.upload_stream({ resource_type: 'image', public_id: customFilename}, (error, result) => {
+     if (error) {
+     console.error('Error during upload:', error);
+     } else {
+     console.log('Upload successful:', result);
+     }
+   });
+ 
+   streamifier.createReadStream(imageBuffer).pipe(stream);
+ 
+   res.redirect("/");
+ 
+ },*/
+
+
+
+  /*carga de producto film*/
+
+  getCrearFilm: function (req, res) {
+    res.render("/CrearFilm")
+  },
+  /*
+    postCrearFilm: function (req, res) {
       const peliculas = JSON.parse(fs.readFileSync(peliculaPath, "utf-8"));
       console.log(req.body);
       const nuevoFilm = {
         id: uuidv4(),
         nombre: req.body.nombre,
-        imagen: result.secure_url || "/img/error-critico.jpg",
+        imagen: req.file
+          ? `/img/${req.file.filename}`
+          : "/img/error-critico.jpg",
+        //en preceso de correccion
         descripcion: req.body.descripcion,
         video: req.body.video,
         fichatecnica: req.body.fichatecnica
+  
       };
       peliculas.push(nuevoFilm);
       const peliculasJSON = JSON.stringify(peliculas, null, " ");
@@ -60,69 +122,7 @@ const controlador = {
           res.redirect("/");
         }
       });
-    }
-  });
-
-  streamifier.createReadStream(imageBuffer).pipe(stream);
-},
-
-
-
-
-
- /*carga de producto film (cloudinary)
- postCrearFilm: (req, res) => {
-	
-  const imageBuffer = req.file.buffer;
-  const customFilename = '';
-
-  const stream = cloudinary.uploader.upload_stream({ resource_type: 'image', public_id: customFilename}, (error, result) => {
-    if (error) {
-    console.error('Error during upload:', error);
-    } else {
-    console.log('Upload successful:', result);
-    }
-  });
-
-  streamifier.createReadStream(imageBuffer).pipe(stream);
-
-  res.redirect("/");
-
-},*/
-
-
-
-  /*carga de producto film*/
-  
-  getCrearFilm: function (req, res) {
-    res.render("/CrearFilm")
-  },
-/*
-  postCrearFilm: function (req, res) {
-    const peliculas = JSON.parse(fs.readFileSync(peliculaPath, "utf-8"));
-    console.log(req.body);
-    const nuevoFilm = {
-      id: uuidv4(),
-      nombre: req.body.nombre,
-      imagen: req.file
-        ? `/img/${req.file.filename}`
-        : "/img/error-critico.jpg",
-      //en preceso de correccion
-      descripcion: req.body.descripcion,
-      video: req.body.video,
-      fichatecnica: req.body.fichatecnica
-
-    };
-    peliculas.push(nuevoFilm);
-    const peliculasJSON = JSON.stringify(peliculas, null, " ");
-    fs.writeFile(peliculaPath, peliculasJSON, error => {
-      if (error) {
-        console.error(error);
-      } else {
-        res.redirect("/");
-      }
-    });
-  },*/
+    },*/
 
   getActualizarFilm: function (req, res) {
     const peliculas = JSON.parse(fs.readFileSync(peliculaPath, "utf-8"));
@@ -137,23 +137,23 @@ const controlador = {
     const peliculas = JSON.parse(fs.readFileSync(peliculaPath, "utf-8"));
     const idM = req.params.id;
     let movie = peliculas.find((pelicula) => pelicula.id == idM);
-  
+
     if (movie) {
       const imageBuffer = req.file.buffer;
       const customFilename = '';
-  
-      const stream = cloudinary.uploader.upload_stream({ resource_type: 'image', public_id: customFilename}, (error, result) => {
+
+      const stream = cloudinary.uploader.upload_stream({ resource_type: 'image', public_id: customFilename }, (error, result) => {
         if (error) {
           console.error('Error:', error);
         } else {
           movie.imagen = result.secure_url || movie.imagen;
-  
+
           /* Guardar los cambios y redireccionar */
           fs.writeFileSync(peliculaPath, JSON.stringify(peliculas, null, " "));
           res.redirect("/");
         }
       });
-  
+
       streamifier.createReadStream(imageBuffer).pipe(stream);
     } else {
       res.send(`
@@ -191,97 +191,108 @@ const controlador = {
 
   ///*peliculas estrenos*///
   estrenos: (req, res) => {
-  res.render('movies/estrenos', { estrenos: ultimosEstrenos })
+
+    db.productoFilm.findAll(
+      {
+        offset: 5,
+        limit: 5
+      }, 
+      { include: [{ association: 'imagen' }] })
+      .then(function (estrenos) {
+        return res.render("movies/estrenos", { estrenos: estrenos })
+      });
+
+    //res.render('movies/estrenos', { estrenos: ultimosEstrenos })
   },
 
-/* peliculas noticias*/
-noticia: (req, res) => {
-  res.render('movies/noticias.ejs', { noticias: slideNoticia });
-},
+  /* peliculas noticias*/
+  noticia: (req, res) => {
+    res.render('movies/noticias.ejs', { noticias: slideNoticia });
+  },
 
   /* peliculas 2023*/
   peliculas2023: (req, res) => {
     res.render('movies/peliculas2023.ejs', { datos: noticiasPelis });
   },
 
-    /* noticias de peliculas slide principal*/
-    detalleNoticia: (req, res) => {
-      let datoP
-      for (let obj of slideNoticia) {
-        if (obj.id === parseInt(req.params.idN)) {
-          console.log(obj)
-          return res.render("movies/detalleNoticia", { data: obj });
-        }
+  /* noticias de peliculas slide principal*/
+  detalleNoticia: (req, res) => {
+    let datoP
+    for (let obj of slideNoticia) {
+      if (obj.id === parseInt(req.params.idN)) {
+        console.log(obj)
+        return res.render("movies/detalleNoticia", { data: obj });
       }
-      // TODO: Aca estaria bueno mandar a una pagina de error
-    },
+    }
+    // TODO: Aca estaria bueno mandar a una pagina de error
+  },
 
-      aspromonte: (req, res) => {
-        res.render(path.resolve(__dirname, '../views/movies/estrenos/aspromonte.ejs'))
-      },
+  aspromonte: (req, res) => {
+    res.render(path.resolve(__dirname, '../views/movies/estrenos/aspromonte.ejs'))
+  },
 
-        blondi: (req, res) => {
-          res.render(path.resolve(__dirname, '../views/movies/estrenos/blondi.ejs'))
-        },
+  blondi: (req, res) => {
+    res.render(path.resolve(__dirname, '../views/movies/estrenos/blondi.ejs'))
+  },
 
-          boogeyman: (req, res) => {
-            res.render(path.resolve(__dirname, '../views/movies/estrenos/boogeyman.ejs'))
-          },
+  boogeyman: (req, res) => {
+    res.render(path.resolve(__dirname, '../views/movies/estrenos/boogeyman.ejs'))
+  },
 
-            elementos: (req, res) => {
-              res.render(path.resolve(__dirname, '../views/movies/estrenos/elementos.ejs'))
-            },
+  elementos: (req, res) => {
+    res.render(path.resolve(__dirname, '../views/movies/estrenos/elementos.ejs'))
+  },
 
-              maremoto: (req, res) => {
-                res.render(path.resolve(__dirname, '../views/movies/estrenos/maremoto.ejs'))
-              },
+  maremoto: (req, res) => {
+    res.render(path.resolve(__dirname, '../views/movies/estrenos/maremoto.ejs'))
+  },
 
-                misantropo: (req, res) => {
-                  res.render(path.resolve(__dirname, '../views/movies/estrenos/misantropo.ejs'))
-                },
+  misantropo: (req, res) => {
+    res.render(path.resolve(__dirname, '../views/movies/estrenos/misantropo.ejs'))
+  },
 
-                  /* rutas carpetas recomendacionesDeSeries*/
-                  recomendacionesSerieNetflix: (req, res) => {
-                    res.render(path.resolve(__dirname, '../views/movies/recomendacionesDeSeries/netflixtop.ejs'))
-                  },
-                    recomendacionesSerieAmazon: (req, res) => {
-                      res.render(path.resolve(__dirname, '../views/movies/recomendacionesDeSeries/amazontop.ejs'))
-                    },
-                      recomendacionesSerisDisney: (req, res) => {
-                        res.render(path.resolve(__dirname, '../views/movies/recomendacionesDeSeries/disneytop.ejs'))
-                      },
+  /* rutas carpetas recomendacionesDeSeries*/
+  recomendacionesSerieNetflix: (req, res) => {
+    res.render(path.resolve(__dirname, '../views/movies/recomendacionesDeSeries/netflixtop.ejs'))
+  },
+  recomendacionesSerieAmazon: (req, res) => {
+    res.render(path.resolve(__dirname, '../views/movies/recomendacionesDeSeries/amazontop.ejs'))
+  },
+  recomendacionesSerisDisney: (req, res) => {
+    res.render(path.resolve(__dirname, '../views/movies/recomendacionesDeSeries/disneytop.ejs'))
+  },
 
-                        /* rutas carpetas top NETFLIX*/
-                        Top1: (req, res) => {
-                          res.render(path.resolve(__dirname, '../views/movies/top/top1netflix.ejs'))
-                        },
-                          Top2: (req, res) => {
-                            res.render(path.resolve(__dirname, '../views/movies/top/top2netflix.ejs'))
-                          },
-                            Top3: (req, res) => {
-                              res.render(path.resolve(__dirname, '../views/movies/top/top3netflix.ejs'))
-                            },
-                              /* rutas carpetas top amazon*/
-                              Topa1: (req, res) => {
-                                res.render(path.resolve(__dirname, '../views/movies/top/top1amazon.ejs'))
-                              },
-                                Topa2: (req, res) => {
-                                  res.render(path.resolve(__dirname, '../views/movies/top/top2amazon.ejs'))
-                                },
-                                  Topa3: (req, res) => {
-                                    res.render(path.resolve(__dirname, '../views/movies/top/top3amazon.ejs'))
-                                  },
+  /* rutas carpetas top NETFLIX*/
+  Top1: (req, res) => {
+    res.render(path.resolve(__dirname, '../views/movies/top/top1netflix.ejs'))
+  },
+  Top2: (req, res) => {
+    res.render(path.resolve(__dirname, '../views/movies/top/top2netflix.ejs'))
+  },
+  Top3: (req, res) => {
+    res.render(path.resolve(__dirname, '../views/movies/top/top3netflix.ejs'))
+  },
+  /* rutas carpetas top amazon*/
+  Topa1: (req, res) => {
+    res.render(path.resolve(__dirname, '../views/movies/top/top1amazon.ejs'))
+  },
+  Topa2: (req, res) => {
+    res.render(path.resolve(__dirname, '../views/movies/top/top2amazon.ejs'))
+  },
+  Topa3: (req, res) => {
+    res.render(path.resolve(__dirname, '../views/movies/top/top3amazon.ejs'))
+  },
 
-                                    /* rutas carpetas top DISNEY*/
-                                    Top1D: (req, res) => {
-                                      res.render(path.resolve(__dirname, '../views/movies/top/top1disney+.ejs'))
-                                    },
-                                      Top2D: (req, res) => {
-                                        res.render(path.resolve(__dirname, '../views/movies/top/top2disney.ejs'))
-                                      },
-                                        Top3D: (req, res) => {
-                                          res.render(path.resolve(__dirname, '../views/movies/top/top3disney.ejs'))
-                                        },
+  /* rutas carpetas top DISNEY*/
+  Top1D: (req, res) => {
+    res.render(path.resolve(__dirname, '../views/movies/top/top1disney+.ejs'))
+  },
+  Top2D: (req, res) => {
+    res.render(path.resolve(__dirname, '../views/movies/top/top2disney.ejs'))
+  },
+  Top3D: (req, res) => {
+    res.render(path.resolve(__dirname, '../views/movies/top/top3disney.ejs'))
+  },
 }
 
 module.exports = controlador;
