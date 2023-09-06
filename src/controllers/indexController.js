d06732f5319a0acfd492ed2209662e7c936c5faa
AbiSulkes <sulkesabi@gmail.com>
2023-09-06 10:17:51 -0300

    combinacion de index lista

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -2,34 +2,32 @@
 let db = require("../database/models");
 const { Op } = require('sequelize');
 
-
 const indexController = {
   index: async (req, res) => {
     try {
-       {
-      let movie = await db.productoFilm.findAll ({
-            limit: 5,
-            order: [['fecha_estreno', 'DESC']] // Ordenar por fecha de estreno en orden descendente.}
-          })
-       
-       console.log("hola", movie);  
+      {
+        let movie = await db.productoFilm.findAll({
+          limit: 5,
+          order: [['fecha_estreno', 'DESC']] // Ordenar por fecha de estreno en orden descendente.}
+        })
 
-          // Consulta para encontrar películas con nombres específicos.
-      const peliculasSlide = await db.productoFilm.findAll({
-        where: {
-          nombre: {
-            [Op.or]: ["The Flash", "Barbie", "La sirenita", "Transformers: el despertar de las bestias", "Rápidos y Furiosos x"]
-          }
-        },
-        order: [['fecha_estreno', 'ASC']] // Ordenar por fecha de estreno en orden ascendente.
-      });
+// Consulta para encontrar películas con nombres específicos.
+        const peliculasSlide = await db.productoFilm.findAll({
+          where: {
+            nombre: {
+              [Op.or]: ["The Flash", "Barbie", "La sirenita", "Transformers: el despertar de las bestias", "Rápidos y Furiosos x"]
+            }
+          },
+          order: [['fecha_estreno', 'ASC']] // Ordenar por fecha de estreno en orden ascendente.
+        });
 
-      
-      res.render("index", { peliculasSlide: peliculasSlide, movie: movie });
-    }} catch (error) {
-      console.log(error) }
-    }    
-    };
+
+        res.render("index", { peliculasSlide: peliculasSlide, movie: movie });
+      }
+     } catch (error) {
+      console.log(error)
+    }}
+};
 
 
 
