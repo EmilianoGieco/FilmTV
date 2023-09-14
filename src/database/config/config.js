13027a3ebd379008880f6bc57112314f1ac30a05
EmilianoGieco <emiliano_jobs@hotmail.com>
2023-09-14 15:22:49 -0300

    configuracion a render

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -15,7 +15,7 @@ module.exports = {
       "database": process.env.DB_DATABASE,
       "host": process.env.DB_HOST,
       "dialect": process.env.DB_DIALECT,
-      "port":process.env.DB_PORT
+      
   },
   "production": {
       "username": process.env.DB_USER,
@@ -23,6 +23,6 @@ module.exports = {
       "database": process.env.DB_DATABASE,
       "host": process.env.DB_HOST,
       "dialect": process.env.DB_DIALECT,
-      "port":process.env.DB_PORT
+      
   }
 }
