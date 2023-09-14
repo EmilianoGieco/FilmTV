e534948e0a408ce90235dc6fcbc886ac48926cb5
EmilianoGieco <emiliano_jobs@hotmail.com>
2023-09-14 15:37:16 -0300

    configuracion a render

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -6,23 +6,23 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT,
-    "port":process.env.DB_PORT
+    "dialect": mysql,
+    "port":3306
   },
   "test": {
       "username": process.env.DB_USER,
       "password": process.env.DB_PASSWORD,
       "database": process.env.DB_DATABASE,
       "host": process.env.DB_HOST,
-      "dialect": process.env.DB_DIALECT,
-      
+      "dialect": mysql,
+      "port":3306
   },
   "production": {
       "username": process.env.DB_USER,
       "password": process.env.DB_PASSWORD,
       "database": process.env.DB_DATABASE,
       "host": process.env.DB_HOST,
-      "dialect": process.env.DB_DIALECT,
-      
+      "dialect": mysql,
+      "port":3306
   }
 }
