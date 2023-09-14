166595f08ed73497eb5ea2b90b4506591fd99350
EmilianoGieco <emiliano_jobs@hotmail.com>
2023-09-14 19:30:04 -0300

    configuracion a render

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const cookies = require('cookie-parser');
 const path = require('path');
 const methodOverride = require("method-override");
-const usuarioLogueoMiddleware = require('./src/middlewares/usuarioLogueoMiddleware');
+const usuarioLogueoMiddleware = require('./middlewares/usuarioLogueoMiddleware');
 
 /* app */
 const app = express();
@@ -22,9 +22,9 @@ app.use(cookies());
 app.use(usuarioLogueoMiddleware);
 
 /* rutas importadas */
-const rutaIndex = require('./src/routers/indexRouter');
-const rutaUsuario = require('./src/routers/usuarioRouter');
-const rutaPeliculas = require('./src/routers/peliculaRouter');
+const rutaIndex = require('./routers/indexRouter');
+const rutaUsuario = require('./routers/usuarioRouter');
+const rutaPeliculas = require('./routers/peliculaRouter');
 
 /* config carpeta public */
 app.use(express.static(path.join(__dirname, '../public')));
@@ -47,13 +47,9 @@ app.use('/', rutaIndex);
 app.use('/usuarios', rutaUsuario);
 app.use('/peliculas', rutaPeliculas);
 
-/* servidor 
+/* servidor */
 const port = 3020
-app.listen(port, () => console.log('Servidor corriendo http://localhost:' + port));*/
-
-const port = process.env.PORT || 3020; // Utiliza el puerto proporcionado por Render o 3020 si no está definido
-app.listen(port, '0.0.0.0', () => console.log('Servidor corriendo en el puerto ' + port));
-
+app.listen(port, () => console.log('Servidor corriendo http://localhost:' + port));
 
 
 
