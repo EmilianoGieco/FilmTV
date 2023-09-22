452d9d40d843376737d00b91ff3f24f9ab5f2969
EmilianoGieco <emiliano_jobs@hotmail.com>
2023-09-22 15:42:12 -0300

    ocultar boton de crearFilm

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -2,14 +2,13 @@
 const db = require("../database/models");
 const { Op } = require('sequelize');
 
+
 const indexController = {
   index: async (req, res) => {
     try {
 
-      /*const usuario = await db.Usuario.findOne({
-        where: { correo: "emiliano_jobs@hotmail.com" }
-      });*/
-
+      const isAdmin = req.session.userLogged && req.session.userLogged.admin === 1;
+      
       // Consulta para obtener las películas más recientes
       const movie = await db.productoFilm.findAll({
         limit: 8,
@@ -26,7 +25,7 @@ const indexController = {
         order: [['fecha_estreno', 'ASC']] // Ordenar por fecha de estreno en orden ascendente.
       });
 
-      res.render("index", { peliculasSlide: peliculasSlide, movie: movie});
+      res.render("index", { peliculasSlide: peliculasSlide, movie: movie, isAdmin:isAdmin });
     } catch (error) {
       console.log(error);
     }
