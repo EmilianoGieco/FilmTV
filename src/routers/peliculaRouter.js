a935b0336b26df06db897acc2826ea01c4de8757
AbiSulkes <sulkesabi@gmail.com>
2023-09-27 19:51:02 -0300

    css de la barra de busqueda completo

--- src/routers/peliculaRouter.js
+++ src/routers/peliculaRouter.js
@@ -1,68 +1,62 @@
-const peliculaController = require ('../controllers/peliculaController');
-const multer = require ("multer");
+const peliculaController = require('../controllers/peliculaController');
+const multer = require("multer");
 const express = require('express');
 const router = express.Router();
 const path = require("path");
 
-console.log( path.join(__dirname,'../../public/img'))
-  const upload = multer();
- 
-/* ruta carpeta detalle pelicula*/
-router.get("/detalle/:id", peliculaController.detallePelicula); 
+console.log(path.join(__dirname, '../../public/img'))
+const upload = multer();
+
+//ruta carpeta detalle pelicula
+router.get("/detalle/:id", peliculaController.detallePelicula);
 //guardado de las calificaciones del usuario
-router.post("/detalle/:id", peliculaController.guardado); 
+router.post("/detalle/:id", peliculaController.guardado);
 
-/* ruta CrearFilm*/
+//ruta CrearFilm
 router.get("/CrearFilm", peliculaController.getCrearFilm);
-router.post("/CrearFilm",upload.single("imagen"), peliculaController.postCrearFilm );
+router.post("/CrearFilm", upload.single("imagen"), peliculaController.postCrearFilm);
 
-/*ruta actualizacionFilm*/
+//ruta actualizacionFilm
 router.get("/actualizarFilm/:id", peliculaController.getActualizarFilm);
-router.put("/actualizarFilm/:id",upload.single("imagen"), peliculaController.postActualizarFilm );
+router.put("/actualizarFilm/:id", upload.single("imagen"), peliculaController.postActualizarFilm);
 
-/* ruta de borrado pelicula */
+//ruta de borrado pelicula
 router.delete("/delete/:id", peliculaController.delete)
 
-/*peliculas con los ultimos estrenos de ahora*/
+//peliculas de "ultimos estrenos"
 router.get("/estrenos", peliculaController.estrenos);
-
 /*detalle de ultimos estrenos*/
 router.get("/estrenos/detalle/:id", peliculaController.detalleEstrenos);
-///////////guardado de las calificaciones del usuario/////////////////
-router.post("/estrenos/detalle/:id", peliculaController.guardadoEstrenos); 
+//guardado de las calificaciones del usuario
+router.post("/estrenos/detalle/:id", peliculaController.guardadoEstrenos);
 
-/*peliculas noticias*/
-router.get('/noticias', peliculaController.noticia );
-
-/*peliculas noticias detalle*/
+//peliculas noticias
+router.get('/noticias', peliculaController.noticia);
+//peliculas noticias detalle
 router.get('/noticias/detalle/:id', peliculaController.detalleNoti);
 
-
-/*buscador*/
-
-//para finalizarlo en un futuro proximo Emi
 /*peliculas del archivo peliculas2023
 router.get('/peliculas2023', peliculaController.peliculas2023 );*/
 
-///* rutas carpetas las 5 mejores peliculas*///
+//rutas carpetas las 5 mejores peliculas (slide)
 router.get("/detalleNoticia/:idN", peliculaController.detalleNoticia);
 
-/* rutas carpetas recomendacionesDeSeries*/
-router.get('/recomendacionesSerieNetflix', peliculaController.recomendacionesSerieNetflix );
-router.get('/recomendacionesSerieAmazon', peliculaController.recomendacionesSerieAmazon );
-router.get('/recomendacionesSerisDisney', peliculaController.recomendacionesSerisDisney );
+//rutas carpetas recomendacionesDeSeries (enrutar como corresponde)
+router.get('/recomendacionesSerieNetflix', peliculaController.recomendacionesSerieNetflix);
+router.get('/recomendacionesSerieAmazon', peliculaController.recomendacionesSerieAmazon);
+router.get('/recomendacionesSerisDisney', peliculaController.recomendacionesSerisDisney);
 
-/* rutas carpetas top NETFLIX*/
-router.get('/Top1', peliculaController.Top1 );
-router.get('/Top2', peliculaController.Top2 );
-router.get('/Top3', peliculaController.Top3 );
+//rutas carpetas top NETFLIX (enrutar como corresponde)
+router.get('/Top1', peliculaController.Top1);
+router.get('/Top2', peliculaController.Top2);
+router.get('/Top3', peliculaController.Top3);
 
-/* rutas carpetas top amazon*/
-router.get('/Topa1', peliculaController.Topa1 );
-router.get('/Topa2', peliculaController.Topa2 );
-router.get('/Topa3', peliculaController.Topa3 );
+//rutas carpetas top amazon (enrutar como corresponde)
+router.get('/Topa1', peliculaController.Topa1);
+router.get('/Topa2', peliculaController.Topa2);
+router.get('/Topa3', peliculaController.Topa3);
 
-/* rutas carpetas top DISNEY*/
+//rutas carpetas top DISNEY (enrutar como corresponde)
 router.get('/Top1D', peliculaController.Top1D);
 router.get('/Top2D', peliculaController.Top2D);
 router.get('/Top3D', peliculaController.Top3D);
